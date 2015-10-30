659c12d493437ebe968c16510cbb7e538f716386
John Rodney <jrodney@poeticsystems.com>
2015-10-30 11:53:04 -0500

    that failed

--- src/ios/apple-pay.m
+++ src/ios/apple-pay.m
@@ -23,35 +23,7 @@
 
 - (void)getAllowsApplePay:(CDVInvokedUrlCommand*)command
 {
-    if (merchantId == nil) {
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"Please call setMerchantId() with your Apple-given merchant ID."];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-        return;
-    }
 
-    PKPaymentRequest *request = [Stripe
-                                 paymentRequestWithMerchantIdentifier:merchantId];
-
-    // Configure a dummy request
-    NSString *label = @"Premium Llama Food";
-    NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:@"10.00"];
-    request.paymentSummaryItems = @[
-                                    [PKPaymentSummaryItem summaryItemWithLabel:label
-                                                                        amount:amount]
-                                    ];
-
-    if ([Stripe canSubmitPaymentRequest:request]) {
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"user has apple pay"];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-    } else {
-#if DEBUG
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"in debug mode, simulating apple pay"];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-#else
-        CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString: @"user does not have apple pay"];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-#endif
-    }
 }
 
 @end
