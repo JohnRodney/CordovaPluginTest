5f42a64f990d2a99f640265a6fd8632c390c93d5
John Rodney <jrodney@poeticsystems.com>
2015-10-30 10:51:37 -0500

    first try at real apple pay code wrapper

--- src/ios/apple-pay.h
+++ src/ios/apple-pay.h
@@ -1,7 +1,20 @@
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+#import <PassKit/PassKit.h>
 #import <Cordova/CDV.h>
 
 @interface ApplePay : CDVPlugin
 
-- (void) greet:(CDVInvokedUrlCommand*)command;
+<
+PKPaymentAuthorizationViewControllerDelegate
+>
+{
+    NSString *merchantId;
+    NSString *callbackId;
+}
+
+- (void)setMerchantId:(CDVInvokedUrlCommand*)command;
+- (void)getAllowsApplePay:(CDVInvokedUrlCommand*)command;
+- (void)getStripeToken:(CDVInvokedUrlCommand*)command;
 
 @end
